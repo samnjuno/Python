e419c50200c6762f64d0d20105f637a285f700fb
samnjuno <samuelnjuno51@gmail.com>
2024-11-02 14:03:15 +0300

    first commit

---
+++ SESSION 1/index.py
@@ -0,0 +1,9 @@
+number = 2
+name = "My name is Samuel Njuno(samnjuno)"
+gender = "male"
+#print (name , gender)
+
+languages =["Dart","Python","Web","DB","SE","E&PD",6,True]
+#print (languages)
+#print (languages[2])
+print(languages[-3])
