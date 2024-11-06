9ea7c7b6c83050d8495eb73432cceb9c7ca8db6b
samnjuno <samuelnjuno51@gmail.com>
2024-11-06 16:16:18 +0300

    made some changes

---
+++ Assignments/input_user.py
@@ -0,0 +1,7 @@
+# Ask the user for their name
+name = "My name is Sam"
+age = 27
+location = "Nairobi,Kenya"
+
+print(f"Name: {name}, Age: {age}, Location: {location}")
+
--- SESSION 1/index.py
+++ SESSION 1/index.py
@@ -1,9 +1,8 @@
 number = 2
 name = "My name is Samuel Njuno(samnjuno)"
 gender = "male"
-#print (name , gender)
-
+print(name,gender)
 languages =["Dart","Python","Web","DB","SE","E&PD",6,True]
 #print (languages)
 #print (languages[2])
-print(languages[-3])
+
